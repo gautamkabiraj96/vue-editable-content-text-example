7dbf358848a60213a1f1ead231b0e6571a9d7795
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-04-18 01:09:16 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h2 contenteditable @input="updateContent($event, 'heading')">
+      {{ contentHeadingTemp }}
+    </h2>
+    <p contenteditable @input="updateContent($event, 'paragraph')">
+      {{ contentMessageTemp }}
+    </p>
+  </div>
+</template>
+
+<script>
+const headingText = "Hello from nightprogrammer.com!";
+const contentText =
+  "Feel free to click on the title above or this paragraph, to make changes!";
+
+export default {
+  data() {
+    return {
+      contentHeading: headingText,
+      contentHeadingTemp: headingText,
+      contentMessage: contentText,
+      contentMessageTemp: contentText,
+    };
+  },
+  methods: {
+    updateContent(e, contentType) {
+      const inputText = e.target.innerText;
+      switch (contentType) {
+        case "heading":
+          this.contentHeading = inputText;
+          break;
+        case "paragraph":
+          this.contentMessage = inputText;
+          break;
+        default:
+          break;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+}
+</style>
